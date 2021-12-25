5475e4586fdab763fdc4c314ab2b6c90429a78fd
salehsami <salehsami017@gamil.com>
2021-12-25 12:30:04 +0500

    First commit

---
+++ lib/Landind_Page.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    Timer(
+      Duration(seconds: 2),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context)
+         => HomePage()
+        ))
+    );
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255,128,88,79),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 90),
+            Image.asset("assets/images/Main_logo1.png"),
+            const SizedBox(height: 0),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+              child: Container(
+                child: const Center(child: Text('UML Diagram Maker', style: TextStyle(
+                  fontSize: 35,
+                  fontFamily: 'Prociono',
+                  fontWeight: FontWeight.w900,
+                  color: Colors.black,
+                ),)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
---
+++ lib/home.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("assets/images/UML_main.png"),
+
+            SizedBox(height: 65),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(60, 0, 0, 0),
+              child: GestureDetector(
+                onTap: null,
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.67,
+                  height: MediaQuery.of(context).size.width*0.11,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255,172,89,71),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: const Center(child: Text('Auto UML Generator', style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Prociono',
+                    fontWeight: FontWeight. bold,
+                    color: Colors.black,
+                  ),)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(60, 0, 0, 0),
+              child: GestureDetector(
+                onTap: null,
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.67,
+                  height: MediaQuery.of(context).size.width*0.11,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255,172,89,71),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: const Center(child: Text('UML Identifier', style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Prociono',
+                    fontWeight: FontWeight. bold,
+                    color: Colors.black,
+                  ),)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(60, 0, 0, 0),
+              child: GestureDetector(
+                onTap: null,
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.67,
+                  height: MediaQuery.of(context).size.width*0.11,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255,172,89,71),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: const Center(child: Text('Picture To UML', style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Prociono',
+                    fontWeight: FontWeight. bold,
+                    color: Colors.black,
+                  ),)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Image.asset("assets/images/UML_main.png"),
+
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(80, 0, 0, 0),
+              child: GestureDetector(
+                onTap: null,
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.60,
+                  height: MediaQuery.of(context).size.width*0.11,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255,172,89,71),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: const Center(child: Text('UML Types', style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Prociono',
+                    fontWeight: FontWeight. bold,
+                    color: Colors.black,
+                  ),)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(80, 0, 0, 0),
+              child: GestureDetector(
+                onTap: null,
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.60,
+                  height: MediaQuery.of(context).size.width*0.11,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255,172,89,71),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: const Center(child: Text('Diagram Editor', style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Prociono',
+                    fontWeight: FontWeight. bold,
+                    color: Colors.black,
+                  ),)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(80, 0, 0, 0),
+              child: GestureDetector(
+                onTap: null,
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.60,
+                  height: MediaQuery.of(context).size.width*0.11,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255,172,89,71),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: const Center(child: Text('Communication', style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Prociono',
+                    fontWeight: FontWeight. bold,
+                    color: Colors.black,
+                  ),)),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(80, 0, 0, 0),
+              child: GestureDetector(
+                onTap: null,
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.60,
+                  height: MediaQuery.of(context).size.width*0.11,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255,172,89,71),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: const Center(child: Text('SignIn/SignUp', style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Prociono',
+                    fontWeight: FontWeight. bold,
+                    color: Colors.black,
+                  ),)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 17),
+            Image.asset("assets/images/UML_main.png"),
+          ],
+        ),
+      ),
+    );
+  }
+}
